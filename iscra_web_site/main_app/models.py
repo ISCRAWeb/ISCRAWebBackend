9b5b718a0a50b9deef4ca410f5f81d67669399ec
Fёdor T <btm.007@mail.ru>
2022-10-28 20:30:57 +0300

    Added two more models

--- iscra_web_site/main_app/models.py
+++ iscra_web_site/main_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class CourseUser(models.Model):
     role = CharField()          # Errr... should we use this?
-    global_account = ForeignKey('User', on_delete=models.CASCADE)
+    global_account = ForeignKey(User, on_delete=models.CASCADE)
 
 class UnitType(models.Model): # Control work, Lecture, etc.
     name = CharField()
@@ -12,11 +12,26 @@ class UnitType(models.Model): # Control work, Lecture, etc.
 class CourseUnit(models.Model):
     name = CharField()          # Defines topic of a unit
     description = TextField()   # Here goes some description
-    unit_type = ForeignKey('UnitType', on_delete=models.SET_NULL)
+    unit_type = ForeignKey(UnitType, on_delete=models.SET_NULL)
+
+class CourseMaterial(models.Model):
+    # hash = CharField()
+    course_unit = ForeignKey(CourseUnit, on_delete=models.CASCADE)
 
 class Course(models.Model):
     name = CharField()
     description = TextField()  
-    lecturer = ForeignKey('CourseUser', on_delete=models.SET_NULL)
-    students = ManyToManyField('CourseUser')
-    program = ManyToManyField('CourseUnit')
+    lecturer = ForeignKey(CourseUser, on_delete=models.SET_NULL)
+    students = ManyToManyField(CourseUser)
+    program = ManyToManyField(CourseUnit)
+    materials = ManyToManyField(CourseMaterial)
+
+class Occasion(models.Model):
+    name = CharField()
+    description = TextField()  
+    date =  models.DateTimeField(blank=True)
+
+class NewsArticle(models.Model):
+    name = CharField()
+    description = TextField()  
+    date =  models.DateTimeField(blank=True)
