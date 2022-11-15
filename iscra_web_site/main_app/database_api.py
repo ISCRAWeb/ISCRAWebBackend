33d4e2d9a48b7475d9c756c843f97ebc2a670c07
Fёdor T <btm.007@mail.ru>
2022-11-15 16:00:34 +0300

    Minor changes

--- iscra_web_site/main_app/database_api.py
+++ iscra_web_site/main_app/database_api.py
@@ -16,7 +16,7 @@ def get_course_user_object(course, django_user):
     """
     Get CourseUser object from course and global account.
     """
-    return models.CourseUser.filter(global_account=django_user).filter(course_set=[course])
+    return models.CourseUser.filter(global_account=django_user).filter(course_set__in=course)
 
 def is_registered(course, django_user):
     """!
@@ -30,8 +30,7 @@ def create_course_user(django_user, roles):
     """!
     Function creates CourseUser object (with saving it to the database).
     """
-    new_course_user = models.CourseUser(global_account=django_user)
-    new_course_user.save()
+    new_course_user = models.CourseUser.objects.create(global_account=django_user)
     new_course_user.roles.add(roles)
     return new_course_user
 
@@ -42,6 +41,7 @@ def add_student(django_user, course, role):
     if is_registered(course, django_user):
         raise AlreadyRegistered('Student is already registered. ')
     new_student = create_course_user(django_user, role)
+    return new_student
 
 def add_lecturer(django_user, course, role):
     """!
@@ -50,17 +50,7 @@ def add_lecturer(django_user, course, role):
     if is_registered(course, django_user):
         raise AlreadyRegistered('Lecturer is already registered. ')
     new_lecturer = create_course_user(django_user, role)
-
-
-def update_user_roles(django_user, course, roles, reset=False):
-    """!
-    Update user roles. If 'reset' is True then old roles are replaced with new.
-    """
-    course_user = get_course_user_object(course, django_user)
-    if reset:
-        course_user.roles.clear()
-    course_user.roles.add(roles)
-    course_user.save()
+    return new_lecturer
 
 def create_course(  name,
                     description,
@@ -74,7 +64,7 @@ def create_course(  name,
     Add new Course. "Name", "description", "status" and "available"
     are mandatory parameters.
     """
-    new_course = models.Course(name=name,
+    new_course = models.Course.objects.create(name=name,
                     description=description,
                     status=status,
                     available=available
@@ -87,15 +77,45 @@ def create_course(  name,
         new_course.program.add(program)
     if materials:
         new_course.materials.add(materials)
-    new_course.save()
+    return new_course
 
-def create_course_unit():
-    
-    
-# ~ def create_course_role(name):
-    # ~ models.CourseRole.objects.create(name=name)
+def create_unit_type(name):
+    """!
+    Add new CourseUnitType.
+    """
+    new_course_unit_type = models.CourseUnitType.objects.create(name=name)
+    return new_course_unit_type
+
+def create_course_unit(name, description, unit_type):
+    """!
+    Add new CourseUnit.
+    """
+    new_course_unit = models.CourseUnit.objects.create(name=name, description=description, unit_type=unit_type)
+
+def create_course_role(name):
+    """!
+    Add new CourseRole.
+    """
+    new_course_role = models.CourseRole.objects.create(name=name)
 
 # ~ def create_course_material():
+
+# ~ def delete_course_user(django_user, course):
     
-# ~ def create_course_user():
+# ~ def delete_course(course):
     
+# ~ def delete_course_unit():
+    
+# ~ def delete_course_unit_type():
+    
+# ~ def delete_course_role():
+    
+
+def update_user_roles(django_user, course, roles, reset=False):
+    """!
+    Update user roles. If 'reset' is True then old roles are replaced with new.
+    """
+    course_user = get_course_user_object(course, django_user)
+    if reset:
+        course_user.roles.clear()
+    course_user.roles.add(roles)
