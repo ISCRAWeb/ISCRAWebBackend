b168863ee39d9387c5331ec1dd5155e0fdaff625
Fёdor T <51658755+Fe-Ti@users.noreply.github.com>
2022-11-19 13:22:12 +0300

    models: database schema implementation
    * Started prototyping databasse models
    
    * Added two more models
    
    * Added available field
    
    * Working on API for database manipulation
    
    * Moved from CharFields to TextFields
    
    * Added User model extension
    
    * Added date fields to the Course model

--- iscra_web_site/main_app/models.py
+++ iscra_web_site/main_app/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Fe-Ti is responsible for bugs in this code
 
-# Create your models here.
+USER_GLOBAL_ROLE_LEN = 30
+
+# ~ COURSE_ROLE_NAME_LEN = 20
+# ~ COURSE_UNITTYPE_NAME_LEN = 20
+
+# ~ COURSE_NAME_LEN = 40
+# ~ COURSE_UNIT_NAME_LEN = 100
+
+# ~ OCCASION_NAME_LEN = 100
+# ~ NEWS_ARTICLE_NAME_LEN = 100
+
+# User model extension
+
+class GlobalUserRole(models.Model):
+    name = CharField(USER_GLOBAL_ROLE_LEN)
+
+class AdditionalUserInfo(models.Model):
+    user = OneToOneField(User, on_delete=models.CASCADE)
+    roles = ManyToManyField(GlobalUserRole, blank=True)
+    description = TextField()
+
+# Course related models
+
+class CourseRole(models.Model):
+    # ~ name = CharField(max_length=COURSE_ROLE_NAME_LEN, unique=True)  # Defines a role of a user
+    name = TextField(unique=True)  # Defines a role of a user
+
+class CourseUser(models.Model):
+    """
+    CourseUser should be unique for each pair of Course and User.
+    The object stores:
+        roles           - a number of roles of the user
+        global_account  - a ForeignKey to django.contrib.auth.models.User
+    """
+    roles = ManyToManyField(CourseRoles)
+    global_account = ForeignKey(User, on_delete=models.CASCADE, unique=False)
+
+class CourseUnitType(models.Model): # Control work, Lecture, etc.
+    # ~ name = CharField(max_length=COURSE_UNITTYPE_NAME_LEN, unique=True)
+    name = TextField(unique=True)
+
+class CourseUnit(models.Model):
+    # ~ name = CharField(max_length=COURSE_UNIT_NAME_LEN) # Defines topic of a unit
+    name = TextField()          # Defines topic of a unit
+    description = TextField()   # Here goes unit description
+    unit_type = ForeignKey(UnitType, on_delete=models.SET_NULL)
+
+class CourseMaterial(models.Model):
+    file_hash = TextField()
+    available = BooleanField()
+    course = ForeignKey(Course, on_delete=models.CASCADE)
+    course_unit = ForeignKey(CourseUnit, on_delete=models.CASCADE)
+    description = TextField()
+    material = FileField(upload_to="uploads/materials/%Y/%m/%d/")
+
+class Course(models.Model):
+    # ~ name = CharField(max_length=COURSE_NAME_LEN)
+    name = TextField()
+    description = TextField()
+    status = SmallIntegerField()        # Announced|Active|Finished
+    available = BooleanField()          # Defines availability of registration
+    lecturers = ManyToManyField(CourseUser, blank=True)
+    students = ManyToManyField(CourseUser, blank=True)
+    program = ManyToManyField(CourseUnit, blank=True)
+    materials = ManyToManyField(CourseMaterial, blank=True)
+    
+    date_of_adt =  models.DateTimeField(blank=True) ## date of announcement
+    date_of_start =  models.DateTimeField(blank=True)
+    date_of_end =  models.DateTimeField(blank=True)
+
+# Other models
+
+class Occasion(models.Model):
+    # ~ name = CharField(max_length=OCCASION_NAME_LEN)
+    name = TextField()
+    description = TextField()  
+    date =  models.DateTimeField(blank=True)
+    available = BooleanField()
+
+class NewsArticle(models.Model):
+    # ~ name = CharField(max_length=NEWS_ARTICLE_NAME_LEN)
+    name = TextField()
+    description = TextField()
+    date =  models.DateTimeField(blank=True)
+    available = BooleanField()
