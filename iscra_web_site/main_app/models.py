a1cd44e92fb6ba1d2868361fe73a7c46849a3865
Fёdor T <btm.007@mail.ru>
2023-03-16 18:11:59 +0300

    Fixed errors in models

--- iscra_web_site/main_app/models.py
+++ iscra_web_site/main_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Fe-Ti is responsible for bugs in this code
 
-USER_GLOBAL_ROLE_LEN = 30
+# ~ USER_GLOBAL_ROLE_LEN = 30
 
 # ~ COURSE_ROLE_NAME_LEN = 20
 # ~ COURSE_UNITTYPE_NAME_LEN = 20
@@ -16,7 +17,8 @@ USER_GLOBAL_ROLE_LEN = 30
 # User model extension
 
 class GlobalUserRole(models.Model):
-    name = models.CharField(max_length=USER_GLOBAL_ROLE_LEN)
+    # ~ name = models.CharField(USER_GLOBAL_ROLE_LEN)
+    name = models.TextField(unique=True)
 
 class AdditionalUserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -47,12 +49,17 @@ class CourseUnit(models.Model):
     # ~ name = CharField(max_length=COURSE_UNIT_NAME_LEN) # Defines topic of a unit
     name = models.TextField()          # Defines topic of a unit
     description = models.TextField()   # Here goes unit description
-    unit_type = models.ForeignKey(CourseUnitType, on_delete=models.SET_NULL, null=True)
+    unit_type = models.ForeignKey(
+                                    CourseUnitType,
+                                    on_delete=models.SET_NULL,
+                                    blank=True,
+                                    null=True
+                                )
 
 class CourseMaterial(models.Model):
     file_hash = models.TextField()
     available = models.BooleanField()
-    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    # ~ course = models.ForeignKey(Course, on_delete=models.CASCADE)
     course_unit = models.ForeignKey(CourseUnit, on_delete=models.CASCADE)
     description = models.TextField()
     material = models.FileField(upload_to="uploads/materials/%Y/%m/%d/")
@@ -63,14 +70,14 @@ class Course(models.Model):
     description = models.TextField()
     status = models.SmallIntegerField()        # Announced|Active|Finished
     available = models.BooleanField()          # Defines availability of registration
-    # lecturers = models.ManyToManyField(CourseUser, blank=True)
-    users = models.ManyToManyField(CourseUser, blank=True)
+    lecturers = models.ManyToManyField(CourseUser, blank=True, related_name="lectured_courses")
+    students = models.ManyToManyField(CourseUser, blank=True, related_name="studied_courses")
     program = models.ManyToManyField(CourseUnit, blank=True)
-    # materials = models.ManyToManyField(CourseMaterial, blank=True)
+    materials = models.ManyToManyField(CourseMaterial, blank=True)
 
-    date_of_adt = models.DateTimeField(blank=True) ## date of announcement
-    date_of_start = models.DateTimeField(blank=True)
-    date_of_end = models.DateTimeField(blank=True)
+    date_of_adt =  models.DateTimeField(blank=True) ## date of announcement
+    date_of_start =  models.DateTimeField(blank=True)
+    date_of_end =  models.DateTimeField(blank=True)
 
 # Other models
 
@@ -78,12 +85,12 @@ class Occasion(models.Model):
     # ~ name = CharField(max_length=OCCASION_NAME_LEN)
     name = models.TextField()
     description = models.TextField()
-    date = models.DateTimeField(blank=True)
+    date =  models.DateTimeField(blank=True)
     available = models.BooleanField()
 
 class NewsArticle(models.Model):
     # ~ name = CharField(max_length=NEWS_ARTICLE_NAME_LEN)
     name = models.TextField()
     description = models.TextField()
-    date = models.DateTimeField(blank=True)
+    date =  models.DateTimeField(blank=True)
     available = models.BooleanField()
